eb1f6b1f1b0bc4dab1658dfa79bae30df6f50ebb
victorGoeman <58217738+victorGoeman@users.noreply.github.com>
2021-11-05 15:39:51 +0100

    Add files via upload

---
+++ HTTP/main.py
@@ -0,0 +1,107 @@
+from flask import Flask, app
+from flask import render_template, url_for, flash, request, redirect, Response
+import sqlite3
+from flask_login import LoginManager, UserMixin, login_required, login_user, logout_user, current_user
+from forms import LoginForm
+
+app = Flask(__name__)
+
+app.debug = True
+
+login_manager = LoginManager(app)
+
+login_manager.login_view = "login"
+
+
+class User(UserMixin):
+    def __init__(self, id, email, password):
+        self.id = (id)
+        self.email = email
+        self.password = password
+        self.authenticated = False
+
+    def is_active(self):
+        return self.is_active()
+
+    def is_anonymous(self):
+        return False
+
+    def is_authenticated(self):
+        return self.authenticated
+
+    def is_active(self):
+        return True
+
+    def get_id(self):
+        return self.id
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    conn = sqlite3.connect('login.db')
+    curs = conn.cursor()
+    curs.execute("SELECT * from login where id = (?)", [user_id])
+    lu = curs.fetchone()
+    if lu is None:
+        return None
+    else:
+        return User(int(lu[0]), lu[1], lu[2])
+
+
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('profile'))
+    form = LoginForm()
+    if form.validate_on_submit():
+        conn = sqlite3.connect('login.db')
+        curs = conn.cursor()
+        data = form.email.data
+        passw = form.password.data
+        print(" \"SELECT * FROM login where email = '%s' and password = '%s'\" " % (data, passw))
+        curs.execute("SELECT * FROM login where email = '%s' and password = '%s'" % (data, passw))
+        data = curs.fetchone()
+        print(data)
+        if data is not None:
+            #user = list(curs.fetchone())
+            Us = load_user(data[0])
+            # if form.email.data == Us.email and form.password.data == Us.password:
+            login_user(Us, remember=form.remember.data)
+            Umail = list({form.email.data})[0].split('@')[0]
+            flash('Logged in successfully ' + Umail)
+            return redirect(url_for('profile'))
+         #   else:
+         #       flash('Login Unsuccessfull.')
+        else:
+            flash("User " + form.email.data + " does not exist")
+    return render_template('login.html', title='Login', form=form)
+
+@app.route("/", methods=['GET'])
+def redirection():
+    return redirect(url_for("login"))
+
+
+@app.route("/profile", methods=['GET'])
+@login_required
+def profile():
+    print("login successful")
+    con = sqlite3.connect("priv.db")
+    con.row_factory = sqlite3.Row
+
+    cur=con.cursor()
+    cur.execute("select * from priv")
+    rows = cur.fetchall()
+
+    return render_template('profile.html', title='profile', rows=rows)
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('login'))
+
+if __name__ == "__main__":
+    app.config.update(dict(
+        SECRET_KEY="powerful secretkey",
+        WTF_CSRF_SECRET_KEY="a csrf secret key"
+    ))
+    app.run(host='0.0.0.0', port=8080, threaded=True)
