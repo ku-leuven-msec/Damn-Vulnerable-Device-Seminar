1e64706d18522cf019023f689658afac2e7280a5
Jorn <jorn.lapon@kuleuven.be>
2021-11-11 16:53:08 +0100

    fix certificate setup

--- certificate_generation/setup_server.sh
+++ certificate_generation/setup_server.sh
@@ -10,12 +10,14 @@ if [ -z "$2" ]
     exit
 fi
 
+CFG_PATH=$(dirname "$0")
+
 mkdir tmp
 
 IP="$1"
-NAME="server_${IP//./_}"
+NAME="server"
 DNS="$2"
-cat server.cnf > "./tmp/$NAME.cnf"
+cat "$CFG_PATH/server.cnf" > "./tmp/$NAME.cnf"
 echo "DNS.2 = $DNS">>"./tmp/$NAME.cnf"
 echo "IP.1 = 127.0.0.1" >> "./tmp/$NAME.cnf" 
 echo "IP.2 = $IP" >> "./tmp/$NAME.cnf" 
@@ -24,10 +26,10 @@ echo "** Setup certificates for server $NAME**"
 echo ""
 
 echo "Create server key pair & certificate request:"
-openssl req -nodes -new -keyout ./cert/$NAME.key -out ./tmp/$NAME.csr -config ./tmp/$NAME.cnf
+openssl req -nodes -new -keyout $NAME.key -out ./tmp/$NAME.csr -config ./tmp/$NAME.cnf
 echo ""
 echo "Generate server certificate"
-openssl x509 -days 3650 -req -in ./tmp/$NAME.csr -CA ./cert/root.cer -CAkey ./cert/root.key -CAcreateserial -out ./cert/$NAME.cer -extfile ./tmp/$NAME.cnf -extensions x509_ext
+openssl x509 -days 3650 -req -in ./tmp/$NAME.csr -CA "$CFG_PATH/cert/root.cer" -CAkey "$CFG_PATH/cert/root.key" -CAcreateserial -out $NAME.cer -extfile ./tmp/$NAME.cnf -extensions x509_ext
 echo ""
 
 rm -r tmp
