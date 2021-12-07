e8eb3e29496d4bc093a3d1def1ebe7cf4d8dd247
victorGoeman <58217738+victorGoeman@users.noreply.github.com>
2021-12-07 10:44:54 +0100

    Deamon fixes - yocto

--- services/http/check_daemon.sh
+++ services/http/check_daemon.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 service=http
-cd `dirname $0`
-ps | grep $service | grep -v grep | grep -v "check" && echo "ok" > /dev/null || (echo "Starting $service service" | python3 server.py >> "$service.log")#!/bin/bash
-service=telnet
 P=`dirname $0`
-ps | grep "$service/server.py" | grep -v grep | grep -v "check"
+ps -aux | grep "$service/server.py" | grep -v grep | grep -v "check"
 if [ $? -eq 0 ]; then
   echo "Already Running" >> "$service.log"
 else
--- services/rest/check_daemon.sh
+++ services/rest/check_daemon.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 service=rest
 P=`dirname $0`
-ps | grep "$service/server.py" | grep -v grep | grep -v "check"
+ps -aux | grep "$service/server.py" | grep -v grep | grep -v "check"
 if [ $? -eq 0 ]; then
   echo "Already Running" >> "$service.log"
 else
