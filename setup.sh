50d3f8154c04044a3a00b13aa72ab869eb09ae2b
DairodR <60882750+DairodR@users.noreply.github.com>
2023-04-03 13:43:03 +0200

    cleanup

--- setup.sh
+++ setup.sh
@@ -166,25 +166,6 @@ setup_certificates() {
 
 full_device_setup(){
   
-  log "Enabling community packages"
-  echo https://dl-cdn.alpinelinux.org/alpine/$ALPINE_VERSION/main > /etc/apk/repositories
-  echo https://dl-cdn.alpinelinux.org/alpine/$ALPINE_VERSION/community >> /etc/apk/repositories
-  echo https://dl-cdn.alpinelinux.org/alpine/edge/testing >> /etc/apk/repositories
-  apk update
-
-  log "Installing python and pip"
-  apk add python3
-  apk add py3-pip
-
-  log "Installing sudo, shadow and bash"
-  apk add sudo
-  apk add shadow
-  apk add bash
-
-  log "Preparing some files and folders for the installation of the DVD"
-  touch /etc/login.defs
-  mkdir /etc/default
-  touch /etc/default/useradd
   
   log "SET Date Time for correct certificate validation"
   set_date
